f4613b3f7ef895d96783191430072ba347585268
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2019-07-19 17:46:28 +0300

    feat(8puzzle): stub for the 4th assignment

---
+++ 04.Puzle/Board.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Board {
+    public static void main(String[] args) {
+        StdOut.println("Test_-_");
+    }
+}
