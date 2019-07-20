1fcde6cdfa23946b6a61406aecf4d5de1690f15e
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2019-07-20 14:46:36 +0300

    chore(8puzzle): Solver stub

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -4,7 +4,34 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.MinPQ;
+
 public class Solver {
+    private final Board initialBoard;
+    private final MinPQ<Board> minPQ;
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        this.initialBoard = initial;
+        this.minPQ = new MinPQ<Board>();
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return this.initialBoard.manhattan() == 0;
+    }
+
+    // min number of moves to solve initial board
+    public int moves() {
+        return -1;
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // test client (see below)
     public static void main(String[] args) {
 
     }
