3a91dd19c9ee638411d3469ff35df3ac34e53fda
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2019-07-26 19:43:30 +0300

    feat(kdtree): PointSET completed 48/100

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -8,6 +8,8 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 
+import java.util.ArrayList;
+
 public class PointSET {
     private SET<Point2D> points;
 
@@ -28,11 +30,23 @@ public class PointSET {
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("point is null");
+        }
+
+        if (this.contains(p)) {
+            return;
+        }
+
         this.points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("point is null");
+        }
+
         return this.points.contains(p);
     }
 
@@ -43,12 +57,39 @@ public class PointSET {
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        if (rect == null) {
+            throw new IllegalArgumentException("rect is null");
+        }
+
+        ArrayList<Point2D> result = new ArrayList<>();
+
+        for (Point2D point : this.points) {
+            if (rect.contains(point)) {
+                result.add(point);
+            }
+        }
+
+        return result;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return p;
+        if (p == null) {
+            throw new IllegalArgumentException("point is null");
+        }
+
+        double minDistance = 1.0;
+        Point2D result = null;
+
+        for (Point2D point : this.points) {
+            double currentDistance = p.distanceTo(point);
+            if (currentDistance < minDistance) {
+                minDistance = currentDistance;
+                result = point;
+            }
+        }
+
+        return result;
     }
 
     // unit testing of the methods (optional)
