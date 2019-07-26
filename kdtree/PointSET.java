f5924c591a583db65ed4b2cd9373904e5339e3a7
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2019-07-26 19:20:51 +0300

    chore(kdtree): stub classes 39/100

---
+++ kdtree/PointSET.java
@@ -0,0 +1,58 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+public class PointSET {
+    private SET<Point2D> points;
+
+    // construct an empty set of points
+    public PointSET() {
+        this.points = new SET<>();
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return this.points.isEmpty();
+    }
+
+    // number of points in the set
+    public int size() {
+        return this.points.size();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        this.points.add(p);
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        return this.points.contains(p);
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return p;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+
+    }
+}
