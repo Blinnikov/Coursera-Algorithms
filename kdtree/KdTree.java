c578d538506ad22142c9cc72b50cffd7c3b3d73f
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2019-07-30 15:32:49 +0300

    feat(kdtree): insert/contains for KdTree 63/100

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -8,9 +8,13 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
+    private Node root;
+    private int n;
+
     // construct an empty set of points
     public KdTree() {
-
+        this.n = 0;
+        this.root = null;
     }
 
     private static class Node {
@@ -18,26 +22,59 @@ public class KdTree {
         private RectHV rect;    // the axis-aligned rectangle corresponding to this node
         private Node lb;        // the left/bottom subtree
         private Node rt;        // the right/top subtree
+
+        public Node(Point2D p) {
+            this.p = p;
+        }
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return this.root == null;
     }
 
     // number of points in the set
     public int size() {
-        return -1;
+        return this.n;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        this.root = insert(this.root, p, true);
+    }
 
+    private Node insert(Node node, Point2D point, boolean isVertical) {
+        if (node == null) {
+            this.n++;
+            // TODO: Set also rect
+            return new Node(point);
+        }
+
+        if (node.p.compareTo(point) == 0) {
+            return node;
+        }
+
+        double cmp = isVertical ? point.x() - node.p.x() : point.y() - node.p.y();
+        if (cmp < 0) node.lb = insert(node.lb, point, !isVertical);
+        else node.rt = insert(node.rt, point, !isVertical);
+        return node;
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        return get(this.root, p, true) != null;
+    }
+
+    private Node get(Node node, Point2D point, boolean isVertical) {
+        if (node == null) return null;
+
+        if (node.p.compareTo(point) == 0) {
+            return node;
+        }
+
+        double cmp = isVertical ? point.x() - node.p.x() : point.y() - node.p.y();
+        if (cmp < 0) return get(node.lb, point, !isVertical);
+        else return get(node.rt, point, !isVertical);
     }
 
     // draw all points to standard draw
